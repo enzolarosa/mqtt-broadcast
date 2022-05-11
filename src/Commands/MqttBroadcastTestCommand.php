518aa88115f38bfbaccf7b26e7aa4602ea17daca
Vincenzo La Rosa <hello@enzolarosa.dev>
2022-05-11 16:00:10 +0200

    Fixed mqtt test command

--- src/Commands/MqttBroadcastTestCommand.php
+++ src/Commands/MqttBroadcastTestCommand.php
@@ -7,18 +7,19 @@ use Illuminate\Console\Command;
 
 class MqttBroadcastTestCommand extends Command
 {
-    protected $signature = 'mqtt-broadcast:test {topic} {message}';
+    protected $signature = 'mqtt-broadcast:test {broker} {topic} {message}';
 
     protected $description = 'Test mqtt server';
 
     public function handle()
     {
+        $broker = $this->argument('broker');
         $topic = $this->argument('topic');
         $message = $this->argument('message');
 
-        $this->comment("I will send `$message` to `$topic` topic");
+        $this->comment("I will send `$message` to `$topic` topic to`$broker` connection");
 
-        MqttMessageJob::dispatch($topic, $message);
+        MqttMessageJob::dispatch($topic, $message, $broker);
 
         $this->comment('Done!');
     }
