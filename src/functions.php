fbfbad833f57612b8338f5e6f2c7a9cdb3fb742f
enzolarosa <hello@enzolarosa.dev>
2024-04-22 15:13:39 +0200

    update

--- src/functions.php
+++ src/functions.php
@@ -4,21 +4,9 @@ use enzolarosa\MqttBroadcast\Jobs\MqttMessageJob;
 use Illuminate\Foundation\Bus\PendingClosureDispatch;
 use Illuminate\Foundation\Bus\PendingDispatch;
 
-if (! function_exists('mqttMessage')) {
-    function mqttMessage(string $topic, mixed $message, string $target, ?string $source = null, ?string $system_id = null, string $broker = 'local', ?int $qos = 0): MqttMessageJob
+if (!function_exists('mqtt')) {
+    function mqtt(string $topic, mixed $message, string $broker = 'local', ?int $qos = 0): PendingDispatch|PendingClosureDispatch
     {
-        return new MqttMessageJob($topic, [
-            'source' => $source ?? config('domes.type').' server',
-            'target' => $target,
-            'data' => $message,
-            'system_uuid' => $system_id,
-        ], $broker, $qos);
-    }
-}
-
-if (! function_exists('mqtt')) {
-    function mqtt(string $topic, mixed $message, string $target, ?string $source = null, ?string $system_id = null, string $broker = 'local', ?int $qos = 0): PendingDispatch|PendingClosureDispatch
-    {
-        return dispatch(mqttMessage($topic, $message, $target, $source, $system_id, $broker, $qos));
+        return dispatch(new MqttMessageJob($topic, $message, $broker, $qos));
     }
 }
